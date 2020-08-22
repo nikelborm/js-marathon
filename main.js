3063c8226fc44440d5bdc546e4edfa12cda0e07c
nikelborm <r0075301net.r@yandex.ru>
2020-08-22 22:01:43 +0300

    Start developing game

--- main.js
+++ main.js
@@ -1,19 +1,63 @@
-function getFormattedPhone( phone ) {
-  let newPhone = '';
-  for ( let i = 0; i < phone.length; i++ ) {
-    if ( i === 2 ) {
-      newPhone += ' (';
-    } else if ( i === 5 ) {
-      newPhone += ') ';
-    } else if ( i === 8 ) {
-      newPhone += '-';
-    } else if ( i === 10 ) {
-      newPhone += '-';
-    }
-    newPhone += phone.charAt( i );
+const $kickBtn = document.getElementById( "btn-kick" );
+const $attackBtn = document.getElementById( "btn-attack" );
+
+const character = {
+  name: "Pikachu",
+  maxHP: 100,
+  realHP: 100,
+  elHPText: document.getElementById( "health-character" ),
+  elHPProgressbar: document.getElementById( "progressbar-character" ),
+};
+const enemy = {
+  name: "Charmande",
+  maxHP: 100,
+  realHP: 100,
+  elHPText: document.getElementById( "health-enemy" ),
+  elHPProgressbar: document.getElementById( "progressbar-enemy" ),
+};
+
+function endFightIf0HP() {
+  if ( enemy.realHP && character.realHP ) return;
+
+  $kickBtn.disabled = true;
+  $attackBtn.disabled = true;
+  if ( !enemy.realHP && !character.realHP ) {
+    alert( "В этой битве нет прогиравших. Только трупы." );
+  } else if ( !enemy.realHP ) {
+    alert( "Бедный " + enemy.name + " прогирал" );
+  } else if ( !character.realHP ) {
+    alert( "Бедный " + character.name + " прогирал" );
   }
-  return newPhone;
 }
-let testPhone = prompt( "Введите номер телефона: " );
-console.log( getFormattedPhone( testPhone ) );
-alert( getFormattedPhone( testPhone ) );
+function renderHP( person ) {
+  // render HP Text
+  person.elHPText.innerText = person.realHP + " / " + person.maxHP;
+  // render HP Progressbar
+  person.elHPProgressbar.style.width = person.realHP + "%";
+}
+function getRandomNum( max ) {
+  return Math.ceil( Math.random() * max );
+}
+function subtractHP( person, count ) {
+  if ( person.realHP > count ) {
+    person.realHP -= count;
+  } else {
+    person.realHP = 0;
+  }
+  renderHP( person );
+}
+function init() {
+  console.log( "Start Game!" );
+  $attackBtn.addEventListener( "click", function() {
+    subtractHP( enemy, getRandomNum( 20 ) );
+    endFightIf0HP();
+  } );
+  $kickBtn.addEventListener( "click", function() {
+    subtractHP( enemy, getRandomNum( 20 ) );
+    subtractHP( character, getRandomNum( 20 ) );
+    endFightIf0HP();
+  } );
+  renderHP( character );
+  renderHP( enemy );
+}
+init();
